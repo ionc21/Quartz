939c908fc48b08b83a3c379d52efe188bbcb9945
Ion <ionc86@gmail.com>
2016-02-24 22:01:01 +0100

    added hibernate entitymanager

--- src/main/java/com/quartz/scheduler/Main.java
+++ src/main/java/com/quartz/scheduler/Main.java
@@ -1,11 +1,12 @@
 package com.quartz.scheduler;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
 	public static void main(final String[] args) throws Exception {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("quartz-jobs.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("quartz-jobs.xml");
 	}
 
 }
